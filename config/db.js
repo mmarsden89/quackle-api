62fd056dbd97ef762d158769ec7f46fbd2c97cdf
mmarsden89 <mattmarsdendev@gmail.com>
2020-11-13 17:42:42 -0800

    atlas update

--- config/db.js
+++ config/db.js
@@ -1,7 +1,7 @@
 "use strict";
 
 // creating a base name for the mongodb
-const mongooseBaseName = "duckPics-api";
+const mongooseBaseName = "quackle";
 
 // create the mongodb uri for development and test
 const database = {
